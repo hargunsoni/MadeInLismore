34544e91f27fe67325f853e8549d09b3633d4fc3
Anannya1234 <ananyasaigal25@gmail.com>
2024-06-03 19:33:31 +1000

    final

--- script.js
+++ script.js
@@ -1,91 +1,43 @@
-function navigateTo(page) {
-    window.location.href = page;
+
+    
+// Set the date we're counting down to
+var countDownDate = new Date("Dec 31, 2024 23:59:59").getTime();
+
+// Update the countdown every 1 second
+var countdownfunction = setInterval(function() {
+    var now = new Date().getTime();
+    var distance = countDownDate - now;
+
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // Update the flip cards with new values
+    updateFlipCard('days', days);
+    updateFlipCard('hours', hours);
+    updateFlipCard('minutes', minutes);
+    updateFlipCard('seconds', seconds);
+
+    if (distance < 0) {
+        clearInterval(countdownfunction);
+        document.getElementById("timer").innerHTML = "EXPIRED";
+    }
+}, 1000);
+
+function updateFlipCard(id, value) {
+    var flipCard = document.getElementById(id).querySelector('.flip-card-inner');
+    var front = flipCard.querySelector('.flip-card-front');
+    var back = flipCard.querySelector('.flip-card-back');
+
+    if (front.innerHTML != value) {
+        flipCard.classList.add('flip');
+        setTimeout(function() {
+            front.innerHTML = value;
+            back.innerHTML = value;
+            flipCard.classList.remove('flip');
+        }, 300);
+    }
 }
 
-function openTab(evt, tabName) {
-    var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-    }
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-    document.getElementById(tabName).style.display = "block";
-    evt.currentTarget.className += " active";
-}
-
-// Set default tab
-document.addEventListener("DOMContentLoaded", function() {
-    if (document.getElementById("Description")) {
-        document.getElementById("Description").style.display = "block";
-    }
-});
-
-
-
-
-
-
-
-
-
-
-//third page second try 
-document.getElementById('buy-now').addEventListener('click', function() {
-    // Navigate to the checkout page
-    window.location.href = 'checkout.html';
-});
-
-function openTab(evt, tabName) {
-    var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-    }
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-    document.getElementById(tabName).style.display = "block";
-    evt.currentTarget.className += " active";
-}
-
-// Set default tab to open
-document.getElementById("Description").style.display = "block";
-
-
-//fourth page 
-document.getElementById('buy-now').addEventListener('click', function() {
-    // Navigate to the checkout page
-    window.location.href = 'checkout.html';
-});
-
-document.getElementById('checkout-button').addEventListener('click', function() {
-    // Navigate to the checkout page
-    window.location.href = 'checkout.html';
-});
-
-document.getElementById('my-cart').addEventListener('click', function() {
-    // Navigate to the checkout page
-    window.location.href = 'checkout.html';
-});
-
-function openTab(evt, tabName) {
-    var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-    }
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-    document.getElementById(tabName).style.display = "block";
-    evt.currentTarget.className += " active";
-}
-
-// Set default tab to open
-document.getElementById("Description").style.display = "block";
 
